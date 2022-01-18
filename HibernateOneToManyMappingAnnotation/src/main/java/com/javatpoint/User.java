fa0ab90d9e41b809c2ee0175111fc7fa82a7a4dc
ChaitanyaWarad <94101135+ChaitanyaWarad@users.noreply.github.com>
2022-01-18 15:54:45 +0530

    Add files via upload

---
+++ HibernateOneToManyMappingAnnotation/src/main/java/com/javatpoint/User.java
@@ -0,0 +1,54 @@
+package com.javatpoint;
+
+import java.util.ArrayList;
+import java.util.Collection;
+
+import javax.persistence.CascadeType;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.OneToMany;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "User")
+public class User {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	@Column(name = "userId")
+	private int userId;
+	@Column(name = "userName")
+	private String userName;
+	@OneToMany(cascade = CascadeType.ALL)
+	@JoinColumn(name = "userId")
+	private Collection<Vehicle> vehicles = new ArrayList<Vehicle>();
+
+	public int getUserId() {
+		return userId;
+	}
+
+	public void setUserId(int userId) {
+		this.userId = userId;
+	}
+
+	public String getUserName() {
+		return userName;
+	}
+
+	public void setUserName(String userName) {
+		this.userName = userName;
+	}
+
+	public Collection<Vehicle> getVehicles() {
+		return vehicles;
+	}
+
+	public void setVehicles(Collection<Vehicle> vehicles) {
+		this.vehicles = vehicles;
+	}
+
+}
