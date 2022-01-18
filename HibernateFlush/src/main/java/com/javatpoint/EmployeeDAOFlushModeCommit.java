371dedca72e8b1792e2365b6bc42fcbe8556b82b
ChaitanyaWarad <94101135+ChaitanyaWarad@users.noreply.github.com>
2022-01-18 15:46:41 +0530

    Add files via upload

---
+++ HibernateFlush/src/main/java/com/javatpoint/EmployeeDAOFlushModeCommit.java
@@ -0,0 +1,34 @@
+package com.javatpoint;
+
+import java.io.Serializable;
+import java.math.BigInteger;
+
+import org.hibernate.FlushMode;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
+import org.hibernate.cfg.Configuration;
+
+public class EmployeeDAOFlushModeCommit {
+
+	public static void main(String[] args) {
+		Configuration cfg = new Configuration();
+		cfg.configure("hibernate.cfg.xml");
+		SessionFactory factory = cfg.buildSessionFactory();
+		Session session = factory.openSession();
+		session.beginTransaction();
+
+		Employee emp = new Employee();
+		emp.setEmpname("chaitu");
+		emp.setEmpsalary("10000");
+
+		session.save(emp);
+
+		// session.getTransaction().commit();
+		long b = (Long) session.createQuery("select count(*) from Employee").setHibernateFlushMode(FlushMode.COMMIT)
+				.getSingleResult();
+		System.out.println("Employee count : " + b);
+		session.getTransaction().commit();
+	}
+
+}
---
+++ HibernateFlush/src/main/java/com/javatpoint/EmployeeDAOSave2.java
@@ -0,0 +1,34 @@
+package com.javatpoint;
+
+import java.io.Serializable;
+import java.math.BigInteger;
+
+import javax.persistence.FlushModeType;
+
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
+import org.hibernate.cfg.Configuration;
+
+public class EmployeeDAOSave2 {
+
+	public static void main(String[] args) {
+		Configuration cfg = new Configuration();
+		cfg.configure("hibernate.cfg.xml");
+		SessionFactory factory = cfg.buildSessionFactory();
+		Session session = factory.openSession();
+		session.beginTransaction();
+
+		Employee emp = new Employee();
+		emp.setEmpname("chaitu");
+		emp.setEmpsalary("10000");
+
+		session.save(emp);
+
+		session.getTransaction().commit();
+		BigInteger k = (BigInteger) session.createNativeQuery("select count(*) from Employee").getSingleResult();
+		System.out.println("Employee Count :" + k);
+		// session.getTransaction().commit();
+	}
+
+}
